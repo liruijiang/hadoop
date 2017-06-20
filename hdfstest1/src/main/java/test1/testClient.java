82e3c69eb08ac5f50c652772db2ac07ff9fa3c1b
Your Name <email@example.com>
2017-06-20 10:48:39 +0800

    testUpdate

--- hdfstest1/src/main/java/test1/testClient.java
+++ hdfstest1/src/main/java/test1/testClient.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.RemoteIterator;
+import org.apache.hadoop.mapred.FileInputFormat;
 
 import java.io.IOException;
 
@@ -13,7 +14,7 @@ import java.io.IOException;
  */
 public class testClient {
     public static void main(String[] args) throws IOException {
-        Configuration configuration =  new Configuration();
+        Configuration configuration =  new Configuration();1
         //获取hdfs客户端对象的时候，需要指定文件系统的类型为hdfs
         configuration.set("fs.defaultFS","hdfs://101.39.43.122:11:9000");
         //或许FileSystem实例对象
